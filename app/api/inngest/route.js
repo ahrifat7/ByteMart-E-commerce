8c582b56704b27aa6563da4581258fd56847c503
ahrifat7 <ahrifat141@gmail.com>
2026-05-03 20:31:53 +0600

    fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
 
+export const dynamic = 'force-dynamic';
+
 // Create an API that serves zero functions
-export const { GET, POST } = serve({
+export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
